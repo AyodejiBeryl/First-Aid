5a3d00bf0ab7f7c8d79d1ec33aa4426418f881ba
Ayodeji <s.ayodejiadeyemo@gmail.com>
2025-11-11 16:59:05 -0600

    requirements added

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="first_aid",
+    version="0.1.0",
+    author="Ayodeji",
+    author_email="s.ayodejiadeyemo@gmail.com",
+    packages=find_packages(),
+    install_requires=[]
+)
